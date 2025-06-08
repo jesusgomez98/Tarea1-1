2c937948c02af5e82b45fbe0a1abdef89b505634
chungo1998prime <jagomezc@unah.hn>
2025-06-08 00:36:04 -0600

    Ejercicio 3

--- Ejercicio 3.js
+++ Ejercicio 3.js
@@ -1,1 +1,17 @@
-hola
+
+function promedioPonderado(notas) {
+  let suma = 0;
+  for (const nota of notas) {
+    suma += nota.valor * nota.peso;
+  }
+  return suma;
+}
+
+const notas = [
+  { valor: 90, peso: 0.5 },
+  { valor: 80, peso: 0.3 },
+  { valor: 70, peso: 0.2 }
+];
+
+const resultado3 = promedioPonderado(notas);
+console.log('Ejercicio 3:', resultado3); 
